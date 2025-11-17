64c70bece8b905b97b04b8dc777d57866300780a
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-11-17 18:03:49 -0500

    07 Finished

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let i = string.length - 1;
+    let OppositeString = '';
+
+    for (; i >= 0; i--){
+        OppositeString += string[i]
+    }
+    return OppositeString
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
