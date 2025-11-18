07b0d28df8c829d27f1e3d0c506f63600288931e
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-11-17 21:55:14 -0500

    11 Finished

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  Celcius = (Fahrenheit - 32) * 5/9
+  return Math.round(Celcius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celcius) {
+  Fahrenheit = (Celcius * 9/5 + 32)
+  return Math.round(Fahrenheit * 10) / 10
 };
 
 // Do not edit below this line
