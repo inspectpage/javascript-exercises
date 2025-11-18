3b78f8984eec749515a9362d1ee46d8369ea9a7a
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-11-17 19:04:05 -0500

    08 Finished

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removelist) {
+    
+    for (const number of arr){
+        for (const remove of removelist){
+            let removeIndex = arr.indexOf(remove);
+            if (removeIndex == -1){
+                continue
+            }
+            arr.splice(removeIndex, 1);
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+[1,2,3,4], 3
