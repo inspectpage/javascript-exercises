bc8dfc58f4ff352f633b9ad8c6e56a7fca07e335
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-11-17 21:13:28 -0500

    09 Finished

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    
+    if (!Number.isInteger(first) || !Number.isInteger(second) || first < 0 || second < 0){
+        return "ERROR"
+    }
+    let range = [];
+    if (first > second){
+        [first, second] = [second, first]
+    }
 
+    for (let i = first; i <= second; i++){
+        range.push(i);
+    }
+
+    let sum = range.reduce((total, currentItem) => {
+        return total + currentItem;
+    }, 0);
+    return sum;
+    
 };
 
 // Do not edit below this line
