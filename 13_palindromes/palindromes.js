e6153c3bcf990b5084594fdce67482fe15d57715
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-12-02 17:45:07 -0500

    13 Finished

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const stringTrim = string.replaceAll(" ", "");
+    const stringLowerCase = stringTrim.toLowerCase();
+    const removePunctuation = stringLowerCase.replace(/[.,!]/g, "")
+    let result = removePunctuation.split("")
+    let reverse = []
 
-};
+    for (let i = -1; i >= -result.length; i--){
+        reverse.push(result.at(i))
+    }
+    result = result.join()
+    reverse = reverse.join()
+
+    if (result === reverse){
+        return true
+    } else{
+        return false
+    }
+}
+
+console.log(palindromes("Hello!"))
 
 // Do not edit below this line
 module.exports = palindromes;
