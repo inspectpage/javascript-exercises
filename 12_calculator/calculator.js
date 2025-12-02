d8e476b22d498dfcd55787f10a4a2d0ab0f09d68
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-12-02 14:06:56 -0500

    12 Finished

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentNum) =>{
+    return Number(total) + Number(currentNum);
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentNum) =>{
+    return Number(total) * Number(currentNum)
+  })
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  let result = 1;
+  if (num === 0){
+    result = 1;
+  }
+  else {
+    for (let i = num; 0 < i; i--){
+      result *= i
+  }}
+  return result
+}
 
 // Do not edit below this line
 module.exports = {
