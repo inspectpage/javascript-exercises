25129d5d49ecfeb65d15c9323e6f54b963066d2b
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-12-02 23:54:14 -0500

    16 Finished

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    let ordered = peopleArray.sort(function(a, b){
+        if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = 2025
+        }
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+    })
+    console.log(ordered)
+    return ordered.at(0)
 };
 
 // Do not edit below this line
