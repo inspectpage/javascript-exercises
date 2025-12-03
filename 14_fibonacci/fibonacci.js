6957e218beec0e683edc31e16b9ace5622f978d9
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-12-02 23:13:03 -0500

    14 Finished

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let array = [1, 1]
+    let first = 1;
+    let second = 1;
+    let result = 2;
+    num = Number(num);
+    if (num == 0){
+        array = [0];
+    }else if (num < 0){
+        return "OOPS"
+    }
+    for (let i = 3; i <= num; i++){
+        result = first + second
+        array.push(result)
+        first = second
+        second = result
+    }
+    return array.at(-1)
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
